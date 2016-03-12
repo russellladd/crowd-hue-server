950f88c65e8fa64fce9d7aaf0e8aa36d2c5c4b5a
Russell Ladd <russell.ladd@grl5.com>
2016-03-11 22:35:33 -0500

    Added put request

--- controllers/main.py
+++ controllers/main.py
@@ -1,4 +1,5 @@
 from flask import *
+from requests import *
 
 main = Blueprint('main', __name__)
 api = Blueprint('api', __name__)
@@ -15,9 +16,28 @@ def api_route():
 	return ('Welcome to the API Index')
 
 
-@api.route('/api/color', methods=['GET', 'POST'])
+@api.route('/api/color', methods=['POST'])
 def api_color_route():
-	return ('Welcome to the API Color')
+
+	body = request.get_json()
+
+	if not 'hue' in body or not 'deviceID' in body:
+		return 'You are missing "hue" or "deviceID" fields', 422
+
+	hue = body['hue']
+	deviceID = body['deviceID']
+
+	if 'room' in body:
+		room = body['room']
+	else:
+		room = 0
+
+	hue = int(hue * 65535)
+	room = room + 1
+
+	requests.put('192.168.86.123/api/169b766c36914e2f2dd44e443bcbf1c7/lights/' + room + '/state', data={"bri": 254, "hue": hue, "sat": 254})
+
+	return 'Success!'
 
 # @main.app_errorhandler(404)
 # def page_not_found(e):
